b4c2163691ca61075546d3da06db78c1c42d51b0
Cleiton Silva <131041820+cleiton-546@users.noreply.github.com>
2023-12-22 07:54:10 -0300

    Update index.jsx

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -26,7 +26,7 @@ export function SignIn() {
         <Container>
             <Form>
                 <h1>Rocket Notes</h1>
-                <p>Aplicação para salvara e gerenciar seus links úteis.</p>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
                 <h2>Faça seu login</h2>
                 <Input 
